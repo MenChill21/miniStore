839b7a51dc792cdd38bc8b74188cae779cfccd68
men <menchill2103@gmail.com>
2024-08-01 23:56:05 +0700

    Add first entities

---
+++ miniStore.Data/EF/miniStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using miniStore.Data.Entities;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace miniStore.Data.EF
+{
+    public class miniStoreContext : DbContext
+    {
+        public miniStoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
