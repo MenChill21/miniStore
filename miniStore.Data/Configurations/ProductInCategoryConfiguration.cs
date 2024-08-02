6597418720b17d38c030596cef89c92b8420d7fd
men <menchill2103@gmail.com>
2024-08-03 00:13:59 +0700

    created entities and first configuration

---
+++ miniStore.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using miniStore.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miniStore.Data.Configurations
+{
+    public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.ToTable("ProductInCategories");
+            builder.HasKey(x => new { x.ProductId, x.CategoryId });
+
+            builder.HasOne(p=>p.Product).WithMany(pc=>pc.ProductInCategories).HasForeignKey(p => p.ProductId);
+
+            builder.HasOne(c=>c.Category).WithMany(pc=>pc.ProductInCategories).HasForeignKey(c => c.CategoryId);
+        }
+    }
+}
--- miniStore.Data/Entities/Product.cs
+++ miniStore.Data/Entities/Product.cs
@@ -13,5 +13,7 @@ namespace miniStore.Data.Entities
         public int ViewCount { set; get; }
         public DateTime DateCreated { set; get; }
         public string SeoAlias { set; get; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
     }
 }
