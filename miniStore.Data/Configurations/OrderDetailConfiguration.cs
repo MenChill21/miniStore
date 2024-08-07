5b7a8f0f9ef5823892c229132a4eb218d30b5590
men <menchill2103@gmail.com>
2024-08-07 23:42:10 +0700

    done database design

--- miniStore.Data/Configurations/OrderDetailConfiguration.cs
+++ miniStore.Data/Configurations/OrderDetailConfiguration.cs
@@ -12,7 +12,7 @@ namespace miniStore.Data.Configurations
         public void Configure(EntityTypeBuilder<OrderDetail> builder)
         {
             builder.ToTable("OrderDetails");
-            builder.HasKey(x => new { x.OrderId, x.ProductId});
+            builder.HasKey(x => new {x.OrderId, x.ProductId});
 
             builder.HasOne(o => o.Order ).WithMany(od=>od.OrderDetails).HasForeignKey(o => o.OrderId);
             builder.HasOne(p => p.Product).WithMany(od => od.OrderDetails).HasForeignKey(p => p.ProductId);
--- miniStore.Data/Entities/OrderDetail.cs
+++ miniStore.Data/Entities/OrderDetail.cs
@@ -7,12 +7,13 @@ namespace miniStore.Data.Entities
     public class OrderDetail
     {
         public int OrderId { set; get; }
+        public Order Order { get; set; }
+
         public int ProductId { set; get; }
+        public Product Product { get; set; }
+
         public int Quantity { set; get; }
         public decimal Price { set; get; }
 
-        public Order Order { get; set; }
-
-        public Product Product { get; set; }
     }
 }
