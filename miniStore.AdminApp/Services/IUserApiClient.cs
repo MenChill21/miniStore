f444578fde0feb2ac0761b345e4df6ff09dfa389
men <menchill2103@gmail.com>
2024-09-23 14:04:53 +0700

    admin app

---
+++ miniStore.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,10 @@
+﻿using miniStore.ViewModels.System.Users;
+using System.Threading.Tasks;
+
+namespace miniStore.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
